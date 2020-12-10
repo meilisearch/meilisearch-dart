0549d0ec96045d829c44d4c5bc0a1bc16fc1de5d
Misir Jafarov <misir.ceferov@gmail.com>
2020-12-10 21:56:08 +0400

    Implement document tests

---
+++ meilisearch/test/documents_test.dart
@@ -0,0 +1,54 @@
+import 'package:test/test.dart';
+
+import 'utils/books.dart';
+import 'utils/client.dart';
+
+void main() {
+  group('Documents', () {
+    setUpClient();
+
+    test('Add documents', () async {
+      var index = await client.createIndex(randomUid());
+      await index.addDocuments(booksWithIntId);
+    });
+
+    test('Add documents', () async {
+      var index = await client.createIndex(randomUid());
+      await index.addDocuments(booksWithIntId, primaryKey: 'book_id');
+    });
+
+    test('Update documents', () async {
+      var index = await createBooksIndex();
+      await index.updateDocuments([
+        {'book_id': 1344, 'title': 'The Hobbit'},
+      ]).waitFor();
+    });
+
+    test('Update documents and pass a primary key', () async {
+      var index = await client.createIndex(randomUid());
+      await index.updateDocuments([
+        {'the_book_id': 1344, 'title': 'The Hobbit'},
+      ], primaryKey: 'the_book_id').waitFor();
+    });
+
+    test('Delete one document', () async {
+      var index = await createBooksIndex();
+      await index.deleteDocument(456).waitFor();
+      expect(index.getDocument(456), throwsException);
+    });
+
+    test('Delete multiple documents', () async {
+      var index = await createBooksIndex();
+      await index.deleteDocuments([456, 4]).waitFor();
+      expect(index.getDocument(4), throwsException);
+      expect(index.getDocument(456), throwsException);
+    });
+
+    test('Delete all documents', () async {
+      var index = await createBooksIndex();
+      await index.deleteAllDocuments().waitFor();
+      var docs = await index.getDocuments();
+      expect(docs, isEmpty);
+    });
+  });
+}
--- meilisearch/test/utils/books.dart
+++ meilisearch/test/utils/books.dart
@@ -1,8 +1,18 @@
+import 'package:meilisearch/meilisearch.dart';
+
+import 'client.dart';
+
 var booksWithIntId = [
   {'book_id': 123, 'title': 'Pride and Prejudice'},
   {'book_id': 456, 'title': 'Le Petit Prince'},
   {'book_id': 1, 'title': 'Alice In Wonderland'},
-  {'book_id': 1344, 'title': 'The Hobbit'},
+  {'book_id': 1344, 'title': 'The Hobit'},
   {'book_id': 4, 'title': 'Harry Potter and the Half-Blood Prince'},
   {'book_id': 42, 'title': 'The Hitchhiker\'s Guide to the Galaxy'}
 ];
+
+Future<MeiliSearchIndex> createBooksIndex() async {
+  var index = await client.createIndex(randomUid());
+  await index.addDocuments(booksWithIntId).waitFor();
+  return index;
+}
