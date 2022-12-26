d80c2d1020d1183f91d799716b671077bb9c94ef
Bruno Casali <brunoocasali@gmail.com>
2022-12-26 17:51:04 -0300

    Move duplicated fields from SearchResult to Searchable abstract class

--- lib/src/search_result.dart
+++ lib/src/search_result.dart
@@ -1,16 +1,11 @@
-class SearchResult {
+import 'package:meilisearch/src/searchable.dart';
+
+class SearchResult extends Searcheable {
   SearchResult({
-    this.hits,
     this.offset,
     this.limit,
-    this.processingTimeMs,
-    this.query,
     this.estimatedTotalHits,
-    this.facetDistribution,
   });
-
-  /// Results of the query
-  final List<Map<String, dynamic>>? hits;
 
   /// Number of documents skipped
   final int? offset;
@@ -18,27 +13,22 @@ class SearchResult {
   /// Number of documents to take
   final int? limit;
 
-  /// Processing time of the query
-  final int? processingTimeMs;
-
-  /// Total number of matches
+  /// Estimated number of matches
   final int? estimatedTotalHits;
 
-  /// Distribution of the given facets
-  final dynamic facetDistribution;
-
-  /// Query originating the response
-  final String? query;
-
   factory SearchResult.fromMap(Map<String, dynamic> map) {
-    return SearchResult(
-      hits: (map['hits'] as List?)?.cast<Map<String, dynamic>>(),
-      query: map['query'] as String?,
+    var result = SearchResult(
       limit: map['limit'] as int?,
       offset: map['offset'] as int?,
-      processingTimeMs: map['processingTimeMs'] as int?,
       estimatedTotalHits: map['estimatedTotalHits'] as int?,
-      facetDistribution: map['facetDistribution'] as dynamic,
     );
+
+    result.hits = (map['hits'] as List?)?.cast<Map<String, dynamic>>();
+    result.query = map['query'] as String?;
+    result.processingTimeMs = map['processingTimeMs'] as int?;
+    result.facetDistribution = map['facetDistribution'] as dynamic;
+    result.matchesPosition = map['_matchesPosition'] as dynamic;
+
+    return result;
   }
 }
