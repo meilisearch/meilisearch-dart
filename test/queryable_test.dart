38671fa3d6f73741d1bfd147f27349b9c5833840
Bruno Casali <brunoocasali@gmail.com>
2022-12-27 17:46:11 -0300

    Add Queryable class to DRY query params classes code

---
+++ test/queryable_test.dart
@@ -0,0 +1,36 @@
+import 'package:meilisearch/src/query_parameters/queryable.dart';
+import 'package:test/test.dart';
+
+class FakeQueryClass extends Queryable {
+  final int? myInteger;
+  final String? myString;
+  final DateTime? myDate;
+  final List<int> myList;
+
+  FakeQueryClass(
+      {this.myInteger, this.myString, this.myDate, this.myList: const []});
+}
+
+void main() {
+  test('responds with non-null values', () {
+    var query = FakeQueryClass(myList: [1, 2], myInteger: 99);
+
+    expect(query.toQuery(), {
+      'myList': '1,2',
+      'myInteger': 99,
+    });
+  });
+
+  test('supports all main types', () {
+    var date = DateTime.now();
+    var query = FakeQueryClass(
+        myList: [1, 2], myInteger: 99, myString: 'foo', myDate: date);
+
+    expect(query.toQuery(), {
+      'myDate': date.toUtc().toIso8601String(),
+      'myString': 'foo',
+      'myList': '1,2',
+      'myInteger': 99,
+    });
+  });
+}
