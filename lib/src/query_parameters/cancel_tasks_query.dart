8b32a01da988f7977cfdf04e4b7d586b774777b4
Bruno Casali <brunoocasali@gmail.com>
2022-12-27 17:46:35 -0300

    Apply Queryable class into CancelTasksQuery

--- lib/src/query_parameters/cancel_tasks_query.dart
+++ lib/src/query_parameters/cancel_tasks_query.dart
@@ -1,4 +1,6 @@
-class CancelTasksQuery {
+import 'package:meilisearch/src/query_parameters/queryable.dart';
+
+class CancelTasksQuery extends Queryable {
   final int? next;
   final DateTime? beforeEnqueuedAt;
   final DateTime? afterEnqueuedAt;
@@ -6,10 +8,10 @@ class CancelTasksQuery {
   final DateTime? afterStartedAt;
   final DateTime? beforeFinishedAt;
   final DateTime? afterFinishedAt;
-  List<int> uids;
-  List<String> statuses;
-  List<String> types;
-  List<String> indexUids;
+  final List<int> uids;
+  final List<String> statuses;
+  final List<String> types;
+  final List<String> indexUids;
 
   CancelTasksQuery(
       {this.next,
@@ -23,24 +25,4 @@ class CancelTasksQuery {
       this.indexUids: const [],
       this.statuses: const [],
       this.types: const []});
-
-  Map<String, dynamic> toQuery() {
-    return <String, dynamic>{
-      'next': this.next,
-      'beforeEnqueuedAt': this.beforeEnqueuedAt,
-      'afterEnqueuedAt': this.afterEnqueuedAt,
-      'beforeStartedAt': this.beforeStartedAt,
-      'afterStartedAt': this.afterStartedAt,
-      'beforeFinishedAt': this.beforeFinishedAt,
-      'afterFinishedAt': this.afterFinishedAt,
-      'uids': this.uids,
-      'indexUids': this.indexUids,
-      'statuses': this.statuses,
-      'types': this.types,
-    }
-      ..removeWhere((key, val) => val == null || (val is List && val.isEmpty))
-      ..updateAll(
-          (key, val) => val is DateTime ? val.toUtc().toIso8601String() : val)
-      ..updateAll((key, val) => val is List ? val.join(',') : val);
-  }
 }
