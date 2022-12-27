1c0416944955c8ec50b40363d94aa5972443d88d
Bruno Casali <brunoocasali@gmail.com>
2022-12-27 17:53:29 -0300

    Apply Queryable class into DocumentsQuery

--- lib/src/query_parameters/documents_query.dart
+++ lib/src/query_parameters/documents_query.dart
@@ -1,15 +1,9 @@
-class DocumentsQuery {
+import 'package:meilisearch/src/query_parameters/queryable.dart';
+
+class DocumentsQuery extends Queryable {
   final int? offset;
   final int? limit;
   final List<String> fields;
 
   DocumentsQuery({this.limit, this.offset, this.fields: const []});
-
-  Map<String, dynamic> toQuery() {
-    return <String, dynamic>{
-      'offset': this.offset,
-      'limit': this.limit,
-      if (this.fields.isNotEmpty) 'fields': fields.join(',')
-    }..removeWhere((_key, value) => value == null);
-  }
 }
