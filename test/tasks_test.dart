f01cc6889c1e6f7ad5ada1f4f20897cc8410f3dc
Bruno Casali <brunoocasali@gmail.com>
2022-12-27 18:53:27 -0300

    Add deleteTasks({DeleteTasksQuery? params}) method to client

--- test/tasks_test.dart
+++ test/tasks_test.dart
@@ -1,4 +1,5 @@
 import 'package:meilisearch/src/query_parameters/cancel_tasks_query.dart';
+import 'package:meilisearch/src/query_parameters/delete_tasks_query.dart';
 import 'package:test/test.dart';
 
 import 'utils/client.dart';
@@ -12,6 +13,22 @@ void main() {
       var response = await client
           .cancelTasks(
               params: CancelTasksQuery(uids: [1, 2], beforeStartedAt: date))
+          .waitFor();
+
+      expect(response.status, 'succeeded');
+      expect(response.details!['originalFilter'],
+          '?beforeStartedAt=${Uri.encodeComponent(date.toUtc().toIso8601String())}&uids=1%2C2');
+    });
+  });
+
+  group('Delete Tasks', () {
+    setUpClient();
+
+    test('delete tasks given an input', () async {
+      var date = DateTime.now();
+      var response = await client
+          .deleteTasks(
+              params: DeleteTasksQuery(uids: [1, 2], beforeStartedAt: date))
           .waitFor();
 
       expect(response.status, 'succeeded');
