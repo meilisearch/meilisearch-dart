1a5857fa4c26e71d29cfefee1440b841b7ad7e5c
Bruno Casali <brunoocasali@gmail.com>
2022-12-27 17:54:01 -0300

    Apply Queryable class into IndexesQuery

--- lib/src/query_parameters/indexes_query.dart
+++ lib/src/query_parameters/indexes_query.dart
@@ -1,16 +1,8 @@
-class IndexesQuery {
+import 'package:meilisearch/src/query_parameters/queryable.dart';
+
+class IndexesQuery extends Queryable {
   final int? offset;
   final int? limit;
 
-  IndexesQuery({
-    this.limit,
-    this.offset,
-  });
-
-  Map<String, dynamic> toQuery() {
-    return <String, dynamic>{
-      'offset': this.offset,
-      'limit': this.limit,
-    }..removeWhere((key, value) => value == null);
-  }
+  IndexesQuery({this.limit, this.offset});
 }
