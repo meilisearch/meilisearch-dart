ddbe6f84bf01bfcb8c43c36a0cf32b56b90c4e8c
Bruno Casali <brunoocasali@gmail.com>
2022-12-27 17:52:42 -0300

    Apply Queryable class into TasksQuery

--- lib/src/query_parameters/tasks_query.dart
+++ lib/src/query_parameters/tasks_query.dart
@@ -1,29 +1,34 @@
-class TasksQuery {
+import 'package:meilisearch/src/query_parameters/queryable.dart';
+
+class TasksQuery extends Queryable {
   final int? from;
   final int? next;
   final int? limit;
-  List<String> statuses;
-  List<String> types;
-  List<String> indexUids;
+  final int? canceledBy;
+  final DateTime? beforeEnqueuedAt;
+  final DateTime? afterEnqueuedAt;
+  final DateTime? beforeStartedAt;
+  final DateTime? afterStartedAt;
+  final DateTime? beforeFinishedAt;
+  final DateTime? afterFinishedAt;
+  final List<int> uids;
+  final List<String> statuses;
+  final List<String> types;
+  final List<String> indexUids;
 
   TasksQuery(
       {this.limit,
       this.from,
       this.next,
+      this.canceledBy,
+      this.beforeEnqueuedAt,
+      this.afterEnqueuedAt,
+      this.beforeStartedAt,
+      this.afterStartedAt,
+      this.beforeFinishedAt,
+      this.afterFinishedAt,
+      this.uids: const [],
       this.indexUids: const [],
       this.statuses: const [],
       this.types: const []});
-
-  Map<String, dynamic> toQuery() {
-    return <String, dynamic>{
-      'from': this.from,
-      'next': this.next,
-      'limit': this.limit,
-      'indexUids': this.indexUids,
-      'statuses': this.statuses,
-      'types': this.types,
-    }
-      ..removeWhere((key, val) => val == null || (val is List && val.isEmpty))
-      ..updateAll((key, val) => val is List ? val.join(',') : val);
-  }
 }
