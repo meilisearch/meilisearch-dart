11e99fa09690faf390a6da73fd1893a6850ee226
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-04-24 09:59:35 +0200

    add parseHighligtableString to the Meili facade class

---
+++ lib/src/utils/_exports.dart
@@ -0,0 +1,1 @@
+export 'higlighted_text.dart';
---
+++ lib/src/utils/higlighted_text.dart
@@ -0,0 +1,11 @@
+/// Class to determine whether a string is highlighted or not
+class MeiliHighlightableStringPart {
+  final String text;
+  final bool isHighlighted;
+
+  const MeiliHighlightableStringPart({
+    required this.text,
+    required this.isHighlighted,
+  });
+}
+
