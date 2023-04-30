cec4c1179ba55f8d77a1be3a009b5d347a6c6f92
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-04-30 06:16:19 +0300

    add assertion to SwapIndex for misuse

--- lib/src/swap_index.dart
+++ lib/src/swap_index.dart
@@ -1,7 +1,11 @@
 class SwapIndex {
   final List<String> indexes;
 
-  SwapIndex(this.indexes);
+  SwapIndex(this.indexes)
+      : assert(
+          indexes.isEmpty || indexes.length == 2,
+          'Indexes must be either empty or have exactly 2 items',
+        );
 
   Map<String, Object> toQuery() {
     return {'indexes': indexes};
