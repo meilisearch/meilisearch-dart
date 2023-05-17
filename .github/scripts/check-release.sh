4439c6fa631ac4c02d52d005b07a37553b717be5
Bruno Casali <brunoocasali@gmail.com>
2023-05-17 15:42:06 -0300

    Remove old checks on check-release.sh

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -4,25 +4,19 @@
 current_tag=$(echo $GITHUB_REF | cut -d '/' -f 3 | sed -r 's/^v//')
 
 file1='pubspec.yaml'
-file2='example/pubspec.yaml'
-file3='example/pubspec.lock'
-file4='README.md'
-file5='lib/src/version.dart'
+file2='README.md'
+file3='lib/src/version.dart'
 changelog_file='CHANGELOG.md'
 ret=0
 
 file_tag1=$(grep '^version: ' $file1 | cut -d ':' -f 2 | tr -d ' ')
-file_tag2=$(grep 'meilisearch: "' $file2 | cut -d ':' -f 2 | tr -d '"' | tr -d ' ')
-file_tag3=$(grep 'meilisearch' -A 6 $file3 | grep 'version: ' | cut -d ':' -f2 | tr -d '"' | tr -d ' ')
-file_tag4=$(grep 'meilisearch: ' $file4 | cut -d '^' -f2)
-file_tag5=$(grep -o "[0-9\.]" $file5 | tr -d '[:space:]')
-if [ "$current_tag" != "$file_tag1" ] || [ "$current_tag" != "$file_tag2" ] || [ "$current_tag" != "$file_tag3" ] || [ "$current_tag" != "$file_tag4" ] || [ "$current_tag" != "$file_tag5" ]; then
+file_tag2=$(grep 'meilisearch: ' $file2 | cut -d '^' -f2)
+file_tag3=$(grep -o "[0-9\.]" $file3 | tr -d '[:space:]')
+if [ "$current_tag" != "$file_tag1" ] || [ "$current_tag" != "$file_tag2" ] || [ "$current_tag" != "$file_tag3" ]; then
   echo "Error: the current tag does not match the version in package file(s)."
   echo "$file1: found $file_tag1 - expected $current_tag"
   echo "$file2: found $file_tag2 - expected $current_tag"
   echo "$file3: found $file_tag3 - expected $current_tag"
-  echo "$file4: found $file_tag4 - expected $current_tag"
-  echo "$file5: found $file_tag5 - expected $current_tag"
   ret=1
 fi
 
