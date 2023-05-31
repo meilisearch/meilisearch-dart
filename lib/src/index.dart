895e0b3e53689720c74ee6fa2e89b4085fbb3dbf
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-05-31 06:49:41 +0300

    lint

--- lib/src/index.dart
+++ lib/src/index.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:meilisearch/meilisearch.dart';
-import 'annotations.dart';
 import 'result.dart';
 import 'tasks_results.dart';
 import 'package:collection/collection.dart';
