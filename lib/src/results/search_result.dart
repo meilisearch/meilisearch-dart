6f0fbd8a3eef357a5b5b9dc8c16d91b68f5f76b7
Ahmed Fwela <ahmednfwela@bdaya-dev.com>
2023-09-01 10:13:09 +0300

    fix(search): remove misplaced `matchesPosition` property from the returned search result. feat(search): added experimental vector. feat(search): added `mapToContainer` extension on Searcheable<T> and its children, to simplify wrapping documents.

--- lib/src/results/search_result.dart
+++ lib/src/results/search_result.dart
@@ -1,26 +1,20 @@
 part of 'searchable.dart';
 
+/// Represents an offset-based search result
 class SearchResult<T> extends Searcheable<T> {
   const SearchResult({
-    List<T> hits = const [],
-    Map<String, Map<String, int>>? facetDistribution,
-    Map<String, List<MatchPosition>>? matchesPosition,
-    int? processingTimeMs,
-    String? query,
-    Map<String, FacetStat>? facetStats,
-    required String indexUid,
-    this.offset,
-    this.limit,
-    this.estimatedTotalHits,
-  }) : super(
-          facetDistribution: facetDistribution,
-          hits: hits,
-          matchesPosition: matchesPosition,
-          processingTimeMs: processingTimeMs,
-          query: query,
-          indexUid: indexUid,
-          facetStats: facetStats,
-        );
+    required super.src,
+    required super.indexUid,
+    required super.hits,
+    required super.facetDistribution,
+    required super.processingTimeMs,
+    required super.query,
+    required super.facetStats,
+    required super.vector,
+    required this.offset,
+    required this.limit,
+    required this.estimatedTotalHits,
+  });
 
   /// Number of documents skipped
   final int? offset;
@@ -36,14 +30,15 @@ class SearchResult<T> extends Searcheable<T> {
     String? indexUid,
   }) {
     return SearchResult(
+      src: map,
+      vector: map['vector'] as List?,
       limit: map['limit'] as int?,
       offset: map['offset'] as int?,
-      estimatedTotalHits: map['estimatedTotalHits'] as int?,
+      estimatedTotalHits: map['estimatedTotalHits'] as int?,      
       hits: _readHits(map),
       query: _readQuery(map),
       processingTimeMs: _readProcessingTimeMs(map),
       facetDistribution: _readFacetDistribution(map),
-      matchesPosition: _readMatchesPosition(map),
       indexUid: indexUid ?? _readIndexUid(map),
       facetStats: _readFacetStats(map),
     );
@@ -54,13 +49,15 @@ class SearchResult<T> extends Searcheable<T> {
     MeilisearchDocumentMapper<T, TOther> mapper,
   ) =>
       SearchResult<TOther>(
+        src: src,
+        facetStats: facetStats,
+        vector: vector,
         indexUid: indexUid,
         facetDistribution: facetDistribution,
         hits: hits.map(mapper).toList(),
         estimatedTotalHits: estimatedTotalHits,
         limit: limit,
         offset: offset,
-        matchesPosition: matchesPosition,
         processingTimeMs: processingTimeMs,
         query: query,
       );
