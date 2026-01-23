481c0b5e52107b9a3c95fdb88a0c16cb94fcc1b9
Madhav Gupta <madhav.gupta7674@gmail.com>
2026-01-24 00:13:02 +0530

    Add more tests

--- test/export_test.dart
+++ test/export_test.dart
@@ -1,4 +1,4 @@
-import 'package:meilisearch/src/query_parameters/_exports.dart';
+import 'package:meilisearch/meilisearch.dart';
 import 'package:test/test.dart';
 import 'utils/client.dart';
 
@@ -14,6 +14,42 @@ void main() {
       expect(result.indexUid, isNull);
       expect(result.status, equals("enqueued"));
       expect(result.type, equals("export"));
+    });
+
+    test('ExportIndexOptions serializes correctly with filterExpression', () {
+      final options = ExportIndexOptions(
+          filterExpression: Meili.gt(
+            Meili.attr('test_attr'),
+            10.toMeiliValue(),
+          ),
+          overrideSettings: true);
+
+      final map = options.asMap();
+
+      expect(map['filter'], '"test_attr" > 10');
+      expect(map['overrideSettings'], true);
+      expect(options.containsFilter, true);
+    });
+
+    test('ExportQuery serializes correctly', () {
+      final options = ExportIndexOptions(
+          filterExpression: Meili.gt(
+            Meili.attr('test_attr'),
+            10.toMeiliValue(),
+          ),
+          overrideSettings: true);
+      final query = ExportQuery(
+          url: 'test_url',
+          apiKey: "test_key",
+          payloadSize: "100 MiB",
+          indexes: {'test_index': options});
+
+      final map = query.buildMap();
+
+      expect(map['url'], 'test_url');
+      expect(map['apiKey'], "test_key");
+      expect(map['payloadSize'], "100 MiB");
+      expect(map['indexes'], {'test_index': options.asMap()});
     });
   });
 }
